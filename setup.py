ef7721983e925b39736e25ec86907bb89be8b5c9
abatten <adamjbatten@gmail.com>
2018-07-30 17:28:29 +1000

    Changed package name to lowercase from all caps

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TOPAZ",
+    name="topaz",
     version="0.0.1",
     author="Adam Batten",
     author_email="adamjbatten@gmail.com",
     description="A plotting package for the Aurora simulations using pynbody",
     long_description=long_description,
-    long_description_content_type="text/restructuredtext",
+    long_description_content_type="text/x-rst",
     url="https://github.com/abatten/TOPAZ",
     packages=setuptools.find_packages(),
     classifiers=(
