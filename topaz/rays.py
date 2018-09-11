f9695b15ad7e982182d1928d2e7f9dd847537546
abatten <adamjbatten@gmail.com>
2018-09-11 16:15:46 +1000

    Created rays.py for interaction with trident

---
+++ topaz/rays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import numpy as np
+from tqdm import tqdm
+import trident
+
+
+def gen_los(snapshot, ray_start, ray_end, **kwargs):
+    return None
