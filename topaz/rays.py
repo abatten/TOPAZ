928a85051d7e7892186de3a64c916c36a5c50a48
abatten <adamjbatten@gmail.com>
2018-09-12 23:13:34 +1000

    Added basic functionallity to rays to test.

--- topaz/rays.py
+++ topaz/rays.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
-from __future__ import print_function
+from __future__ import print_function, division
 
 import numpy as np
 from tqdm import tqdm
 import trident
 
 
-def gen_los(snapshot, ray_start, ray_end, **kwargs):
-    return None
+def gen_ray(fn, ray_start, ray_end, line_list=["H", "He"], 
+            field_list=[], filename="ray.h5",  **kwargs):
+
+
+    field_list.extend([('PartType0', 'SmoothingLength'),
+                       ('PartType0', 'ParticleIDs')])
+
+    ray = trident.make_simple_ray(fn, 
+                                  start_position=ray_start,
+                                  end_position=ray_end,
+                                  lines=line_list,
+                                  ftype='PartType0',
+                                  fields=field_list,
+                                  data_filename=filename)
+
+
+def rand_los(fn, output_data_dir, full_width=True):
+    if full_width:
+        ds = yt.load(fn)
+        width = ds.properties
+
+
+def gen_n_los(fn, n, rand=True):
+
+    for i in range(n):
+        gen_los(fn,
+                )
